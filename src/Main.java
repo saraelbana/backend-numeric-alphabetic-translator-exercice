c88f60083eb7b76e770030505d8b4e9cc121e5c8
saraelbana <matika20@gmail.com>
2023-11-28 18:06:51 +0100

    first commit includes setting the map initial values

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
         // Hier mag je je code schrijven voor de hoofd-opdracht
 
+        HashMap<Integer, String> numericAlphabeticMap = new HashMap<>();
+        setMapInitialValues(numericAlphabeticMap);
 
+
+
+    }
+
+    private static void setMapInitialValues(HashMap<Integer, String> numericAlphabeticMap) {
+
+        numericAlphabeticMap.put(0,"zero");
+        numericAlphabeticMap.put(1,"one");
+        numericAlphabeticMap.put(2,"two");
+        numericAlphabeticMap.put(3,"three");
+        numericAlphabeticMap.put(4,"four");
+        numericAlphabeticMap.put(5,"five");
+        numericAlphabeticMap.put(6,"six");
+        numericAlphabeticMap.put(7,"seven");
+        numericAlphabeticMap.put(8,"eight");
+        numericAlphabeticMap.put(9,"nine");
     }
 }
